53c9584c8a01408cbe5e60dcce69651b99779262
Egor-Cherepanov <105498881+Egor-Cherepanov@users.noreply.github.com>
2025-01-24 22:37:35 +0300

    working render

--- src/SecondExe.jsx
+++ src/SecondExe.jsx
@@ -3,7 +3,9 @@ import styles from "./App.module.css"
 // import { use } from 'react'
 
 export function App() {
-  const [todos, setTodoos] = useState([])
+  const [todos, setTodos] = useState([])
+  const [sortedTodos, setSortedTodos] = useState([])
+
   const [refreshProducts, setRefreshProducts] = useState(false)
   const [inputError, setInputError] = useState(null)
   //CRUD
@@ -13,17 +15,19 @@ export function App() {
   const [isCreating, setIsCreating] = useState(false)
   const [isUpdating, setIsUpdating] = useState(false)
   //options
-  const [isFinding, setIsFinding] = useState(false)
-  const [findingInput, setFindingInput] = useState("")
-  const [sortTodoos, setSortTodoos] = useState([])
-  const [sortTodoosUpdated, setSortTodoosUpdated] = useState(false)
+  const [isFiltered, setIsFiltered] = useState(false)
+  const [filteredInput, setFilteredInput] = useState("")
+  // const [filteredTodoos, setFilteredTodoos] = useState("")
+
+  const [alfabetTodoosUpdated, setAlfabetTodoosUpdated] = useState(false)
 
   useEffect(() => {
     setIsLoading(true)
     fetch("http://localhost:3005/todoos")
       .then((loadedData) => loadedData.json())
-      .then((loadedTodoos) => {
-        setTodoos(loadedTodoos)
+      .then((loadedTodos) => {
+        setTodos(loadedTodos)
+        setSortedTodos(loadedTodos)
       })
       .finally(() => setIsLoading(false))
   }, [refreshProducts])
@@ -37,7 +41,7 @@ export function App() {
     setInputError(newError)
   }
 
-  const replaceAddTodoo = () => {
+  const replaceAddTodo = () => {
     if (inputValue === "") {
       return setInputError("Введите текст задачи")
     }
@@ -57,16 +61,18 @@ export function App() {
       .finally(() => {
         setIsCreating(false)
         setInputValue("")
+        setAlfabetTodoosUpdated(false)
       })
   }
 
-  const startUpdateTodoo = ({ target }) => {
+  const startUpdateTodo = ({ target }) => {
     setInputValue(target.dataset.text)
     setIsUpdating(true)
     setActualId(target.dataset.id)
+    setAlfabetTodoosUpdated(false)
   }
 
-  const requestUpdateTodoo = () => {
+  const requestUpdateTodo = () => {
     if (inputValue === "") {
       return setInputError("Введите текст задачи")
     }
@@ -87,10 +93,11 @@ export function App() {
         setIsUpdating(false)
         setInputValue("")
         setActualId(0)
+        setAlfabetTodoosUpdated(false)
       })
   }
 
-  const requestDeleteTodoo = ({ target }) => {
+  const requestDeleteTodo = ({ target }) => {
     fetch(`http://localhost:3005/todoos/${target.dataset.id}`, {
       method: "DELETE",
     })
@@ -98,20 +105,53 @@ export function App() {
       .then((response) => {
         console.log("Задача удалена, ответ сервера: ", response)
       })
-      .finally(() => setRefreshProducts(!refreshProducts))
+      .finally(() => {
+        setRefreshProducts(!refreshProducts)
+        setAlfabetTodoosUpdated(false)
+      })
   }
 
-  const searchTodoo = (formData) => {
-    console.log(formData)
+  const debounce = (callBack, delay = 1000) => {
+    let timeout
+    return (...arg) => {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {
+        callBack(...arg)
+      }, delay)
+    }
   }
 
-  const alfabetSortTodoos = () => {
-    setSortTodoos(todos.sort((a, b) => a.title.localeCompare(b.title)))
-    setSortTodoosUpdated(true)
+  const filterTodos = ({ target }) => {
+    setAlfabetTodoosUpdated(false)
+    setFilteredInput(target.value)
+    console.log(sortedTodos)
+
+    if (sortedTodos.length === 0) {
+      setSortedTodos(todos)
+    }
+
+    const debouncedSetSortedTodos = debounce((value) => {
+      setSortedTodos(
+        sortedTodos.filter((todo) => {
+          return todo.title.toLowerCase().includes(value.toLowerCase())
+        })
+      )
+    })
+
+    debouncedSetSortedTodos(target.value)
+  }
+
+  const alfabetSortTodos = () => {
+    if (alfabetTodoosUpdated) {
+      setRefreshProducts(todos)
+    } else {
+      setSortedTodos(sortedTodos.sort((a, b) => a.title.localeCompare(b.title)))
+    }
+    setAlfabetTodoosUpdated(!alfabetTodoosUpdated)
   }
 
   return (
-    <div className={styles.todoApp} onClick={() => setSortTodoosUpdated(false)}>
+    <div className={styles.todoApp}>
       <h1>Список дел</h1>
       {inputError && <div className={styles.inputError}>{inputError}</div>}
       <div className={styles.todoForm}>
@@ -126,8 +166,8 @@ export function App() {
             />
             <button
               className={styles.addButton}
-              onClick={requestUpdateTodoo}
-              disabled={isCreating || !!inputError}
+              onClick={requestUpdateTodo}
+              disabled={!!inputError}
             >
               Отредактировать задачу
             </button>
@@ -143,7 +183,7 @@ export function App() {
             />
             <button
               className={styles.addButton}
-              onClick={replaceAddTodoo}
+              onClick={replaceAddTodo}
               disabled={isCreating || !!inputError}
             >
               Добавить задачу
@@ -152,13 +192,13 @@ export function App() {
         )}
       </div>
 
-      {isFinding ? (
-        <form className={styles.todooOptions} action={todos}>
+      {isFiltered ? (
+        <form className={styles.todooOptions}>
           <input
             type="search"
             className={styles.todoInput}
-            value={findingInput}
-            onChange={(e) => setFindingInput(e.value)}
+            value={filteredInput}
+            onChange={filterTodos}
             placeholder="Введите задачу..."
           />
           <button type="submit" className={styles.optionsButton}>
@@ -167,12 +207,14 @@ export function App() {
         </form>
       ) : (
         <div className={styles.todooOptions}>
-          <button className={styles.optionsButton} onClick={alfabetSortTodoos}>
-            Отсортировать задачи по алфавиту
+          <button className={styles.optionsButton} onClick={alfabetSortTodos}>
+            {alfabetTodoosUpdated
+              ? "Изначальная сортировка"
+              : "Отсортировать задачи по алфавиту"}
           </button>
           <button
             className={styles.optionsButton}
-            onClick={() => setIsFinding(true)}
+            onClick={() => setIsFiltered(true)}
           >
             Найти задачу
           </button>
@@ -183,11 +225,11 @@ export function App() {
         <div className={styles.loader}></div>
       ) : (
         <ul className={styles.todoList}>
-          {(sortTodoosUpdated ? sortTodoos : todos).map((todo) => (
+          {sortedTodos.map((todo) => (
             <li key={todo.id} className={styles.todoItem}>
               <span className={styles.todoText}>{todo.title}</span>
               <button
-                onClick={startUpdateTodoo}
+                onClick={startUpdateTodo}
                 data-id={todo.id}
                 className={styles.updateButton}
                 data-text={todo.title}
@@ -195,7 +237,7 @@ export function App() {
                 Редактировать задачу
               </button>
               <button
-                onClick={requestDeleteTodoo}
+                onClick={requestDeleteTodo}
                 data-id={todo.id}
                 className={styles.deleteButton}
               >
@@ -205,6 +247,7 @@ export function App() {
           ))}
         </ul>
       )}
+      {}
     </div>
   )
 }
